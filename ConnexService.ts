ee112abeffce6606677598b9f8afd6891e49ec7f
Bradley <bradleysuira@gmail.com>
2019-03-07 11:21:57 -0500

    modified connex service to setup contracts

--- ConnexService.ts
+++ ConnexService.ts
@@ -1,13 +1,27 @@
+import { ContractSetting } from "./types";
+
 // eslint-disable-next-line spaced-comment
 /// <reference types="@vechain/connex" />
 export const ACCOUNT_CHANGES_INTERVAL = 1_000;
 
 export class ConnexService {
+  public static contractInstances: any = {};
+  
   public static instance: Connex;
 
   public static defaultAccount: string;
 
   public static chainTag: string;
+
+  public static contracts<T>(name: string): T {
+    return ConnexService.contractInstances[name] as T;
+  }
+
+  public static setupContracts(settings: ContractSetting[]): void {
+    settings.forEach((s: ContractSetting) => {
+      ConnexService.contractInstances[s.name] = new s.contract();
+    });
+  }
 
   public static getDefaultAccountSigningService(): Connex.Vendor.TxSigningService {
     const signingService = ConnexService.instance.vendor.sign('tx');
